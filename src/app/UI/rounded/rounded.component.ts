555b36cf014ea897321e5a9f8bb6cd35cd1a14af
sunilpanch <sunilpanchal123.sp@gmail.com>
2023-03-27 17:34:46 +0530

    Header component completed

---
+++ src/app/UI/rounded/rounded.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-rounded',
+  templateUrl: './rounded.component.html',
+  styleUrls: ['./rounded.component.scss'],
+})
+export class RoundedComponent {
+  src: string = './../../../assets/images/rounded/';
+  @Input() title: string | undefined;
+  @Input() alt: string | undefined;
+  @Input()
+  set fileName(val: string) {
+    this.src += val;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { RoundedComponent } from './UI/rounded/rounded.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    RoundedComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
