a908881a5cfbe14d047268f67c0d4db8aaa759e6
sunilpanch <sunilpanchal123.sp@gmail.com>
2023-03-30 10:15:44 +0530

    completed

--- src/app/product-page/product-page.module.ts
+++ src/app/product-page/product-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductPageComponent } from './product-page.component';
 import { UIModule } from '../UI/ui.module';
+import { ProductPageRoutingModule } from './product-page-routing.module';
 
 @NgModule({
   declarations: [ProductPageComponent],
-  imports: [CommonModule, UIModule],
+  imports: [CommonModule, UIModule, ProductPageRoutingModule],
 })
 export class ProductPageModule {}
